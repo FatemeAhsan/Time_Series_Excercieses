7002f6906d0495828e2163c192b144df2fccddf0
Fatemeh Ahsan <fteme.ahsan@gmail.com>
2023-11-09 16:42:55 +0330

    add utils

---
+++ utils.py
@@ -0,0 +1,14 @@
+import doctest
+
+def print_doctest(func):
+    lines = doctest.script_from_examples(func.__doc__).splitlines()
+    ex_indx = lines.index('#         Example::')
+    print('\n'.join(lines[ex_indx + 2:]))
+
+
+def print_locals(name, locals_):
+    for k, v in locals_.items():
+        try:
+          print(name, '-', k, '=', v, '-', v.shape)
+        except:
+          print(name, '-', k, '=', v)
