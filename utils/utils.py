a8fa30f7376956d1ef903c2f8313e6373b1fa425
Fatemeh Ahsan <fteme.ahsan@gmail.com>
2023-11-13 23:05:49 +0330

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -5,6 +5,8 @@ def print_doctest(func):
     ex_indx = lines.index('#         Example::')
     print('\n'.join(lines[ex_indx + 2:]))
 
+def print_test_rs(verbose=True):
+    doctest.testmod(verbose=verbose)
 
 def print_locals(name, locals_):
     for k, v in locals_.items():
