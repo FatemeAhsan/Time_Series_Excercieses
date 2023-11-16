cb3875eff50f8a2d2eb0364982bf11dbfdab0322
Fatemeh Ahsan <fteme.ahsan@gmail.com>
2023-11-16 23:57:48 +0330

    add files

--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,12 @@
 import doctest
 
+def print_test_rs(verbose=True):
+    doctest.testmod(verbose=verbose)
+
 def print_doctest(func):
     lines = doctest.script_from_examples(func.__doc__).splitlines()
     ex_indx = lines.index('#         Example::')
     print('\n'.join(lines[ex_indx + 2:]))
-
-def print_test_rs(verbose=True):
-    doctest.testmod(verbose=verbose)
 
 def print_locals(name, locals_):
     for k, v in locals_.items():
